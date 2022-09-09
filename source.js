8a6364e9db26c522563a66ee1a900dff0e7b8ee7
Mahzan <110054003+mahzan123@users.noreply.github.com>
2022-09-09 21:31:58 +0800

    Update source.js

--- source.js
+++ source.js
@@ -12,9 +12,41 @@ function chat() {
   open(whatsapp, "_blank")
 }
 
-/*CATEGORY*/
+/*my brand*/
+function img() {
+  alert('Yah kok di pencet 😜')
+}
+
+/*PRODUCT*/
 function beli() {
   const message = `Halo saya mau beli, ${'Harga bajunya berapa kaka'}`
   const whatsapp = `https://wa.me/6285933650257?text=${message}`
   open(whatsapp, "_blank")
 }
+
+/*CONTACT*/
+function instagram() {
+  alert('Yah mimin belum punya akun tuh.. 😌')
+}
+
+function telegram() {
+  alert('Yah mimin belum punya akun tuh.. 😌')
+}
+
+function twitter() {
+  alert('Yah mimin belum punya akun tuh.. 😌')
+}
+
+function linkedin() {
+  alert('Yah mimin belum punya akun tuh.. 😌')
+}
+
+
+
+/*menu toggle*/
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function() {
+  nav.classList.toggle("slide");
+})
